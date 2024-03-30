bc28fede5e4a7ccb50c2db3e1476e7ccd9f6ac15
gabrielamarante1 <153243893+gabrielamarante1@users.noreply.github.com>
2024-03-29 23:23:20 -0300

    entregafinal

--- proyecto.js
+++ proyecto.js
@@ -2,7 +2,7 @@ const contenidoTienda = document.getElementById("contenido-tienda")
 const verCarrito = document.getElementById("verCarrito")
 const contenedorModal = document.getElementById("contenedor-modal")
 
-let carrito = []
+let carrito = JSON.parse(localStorage.getItem("carro")) || [];
 productos.forEach((producto)=> {
     let contenidoProducto = document.createElement("div")
     contenidoProducto.className = "tarjeta";
@@ -17,50 +17,31 @@ productos.forEach((producto)=> {
     botonComprar.className = "boton-comprar";
     contenidoProducto.append(botonComprar);
 
-    botonComprar.addEventListener("click", () =>{
+    botonComprar.addEventListener("click", () => {
+    const repeat = carrito.some((repeatproduct) => repeatproduct.id === producto.id); 
+    if (repeat) {
+        carrito.forEach((prod) => {
+        if(prod.id === producto.id) {
+            prod.cantidad++;
+        }
+    });
+    } else {
+        
         carrito.push({
             id: producto.id,
             img: producto.img,
             nombre: producto.nombre,
             precio: producto.precio,
+            cantidad: producto.cantidad,
         });
-    })
-
-});
-verCarrito.addEventListener("click", () => {
-    contenedorModal.innerHTML = ""
-    contenedorModal.style.display = "flex"
-    const cabeceraModal = document.createElement("div");
-    cabeceraModal.className = "cabecera-modal"
-    cabeceraModal.innerHTML = `
-        <h1 class="titulo-cabecera-modal">Carrito de Compras</h1>
-    `;
-    contenedorModal.append(cabeceraModal);
-
-    const botonCerrarModal = document.createElement("h1");
-    botonCerrarModal.innerText = "X";
-    botonCerrarModal.className = "boton-cerrar-cabecera-modal";
-
-    botonCerrarModal.addEventListener("click", () => {
-        contenedorModal.style.display = "none";
+        
+        }
+        savelocal();
     });
+})
 
-    cabeceraModal.append(botonCerrarModal);
-    carrito.forEach((producto) => {
+const savelocal = () => {
+localStorage.setItem("carro", JSON.stringify (carrito));
+};
 
-    let contenidoCarrito = document.createElement("div");
-    contenidoCarrito.className = "contenido-modal";
-    contenidoCarrito.innerHTML = `
-        <img src="${producto.img}">
-        <h3>${producto.nombre}</h3>
-        <p>${producto.precio} $</p>
-    `;
-        contenedorModal.append(contenidoCarrito);
-    });
-
-    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
-    const totalCompra = document.createElement("div");
-    totalCompra.className = "total-contenido";
-    totalCompra.innerHTML = `Total a Pagar: ${total} $` ;
-    contenedorModal.append(totalCompra);
-});
+JSON.parse(localStorage.getItem("carro"));
